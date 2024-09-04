501cdeaa37834471748fd3965272470ff141153a
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-09-04 16:23:57 +0700

    Perkenalan Miqba + TTL

--- Biodata.java
+++ Biodata.java
@@ -2,6 +2,7 @@ public class Biodata{
     public static void main(String[] args){
         System.out.println("Perkenalan: ");
         System.out.println("\nNama\t: Abdul Ghofur Almiqbadi");
+        System.out.println("TTL\t: Surabaya, 20 Maret 2005");
         System.out.println("NIM\t: 244107020155");
         System.out.println("Kelas\t: TI-1A");
         System.out.println("No Absen: 01");
