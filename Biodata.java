9245e27220d0600b2b2ee19aa09308c005cc4e22
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-09-04 15:57:23 +0700

    Update Penambahan Biodata di Main

---
+++ Biodata.java
@@ -0,0 +1,11 @@
+public class Biodata{
+    public static void main(String[] args){
+        System.out.println("Perkenalan: -");
+        System.out.println("\nNama\t: -");
+        System.out.println("NIM\t: -");
+        System.out.println("Kelas\t: -");
+        System.out.println("No Absen: -");
+        System.out.println("Prodi\t: -");
+        System.out.println("Alamat\t: -");
+    }
+}
