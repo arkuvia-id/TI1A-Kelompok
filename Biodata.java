db9b9fb41d87047a9965334d23d8c1cd4ebb8ce9
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2024-09-04 16:19:44 +0700

    hai lin

--- Biodata.java
+++ Biodata.java
@@ -1,11 +1,11 @@
 public class Biodata{
     public static void main(String[] args){
-        System.out.println("Perkenalan: -");
-        System.out.println("\nNama\t: -");
-        System.out.println("NIM\t: -");
-        System.out.println("Kelas\t: -");
-        System.out.println("No Absen: -");
-        System.out.println("Prodi\t: -");
-        System.out.println("Alamat\t: -");
+        System.out.println("Perkenalan: ");
+        System.out.println("\nNama\t: Linda octaviani");
+        System.out.println("NIM\t: 244107020002");
+        System.out.println("Kelas\t: TI-1A");
+        System.out.println("No Absen: 15");
+        System.out.println("Prodi\t: Teknik Informatika");
+        System.out.println("Alamat\t: Jl. Semanggi Barat No. 22 Lowokwaru");
     }
 }
