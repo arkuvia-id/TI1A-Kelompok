e1cb9d63dc73ee2afdc81ec2e62d1d22a307ca31
arkuvia-id <abdulghofuralmiqbadi2@gmail.com>
2024-09-04 16:16:22 +0700

    Perkenalan Miqba

--- Biodata.java
+++ Biodata.java
@@ -1,11 +1,11 @@
 public class Biodata{
     public static void main(String[] args){
-        System.out.println("Perkenalan: -");
-        System.out.println("\nNama\t: -");
-        System.out.println("NIM\t: -");
-        System.out.println("Kelas\t: -");
-        System.out.println("No Absen: -");
-        System.out.println("Prodi\t: -");
-        System.out.println("Alamat\t: -");
+        System.out.println("Perkenalan: ");
+        System.out.println("\nNama\t: Abdul Ghofur Almiqbadi");
+        System.out.println("NIM\t: 244107020155");
+        System.out.println("Kelas\t: TI-1A");
+        System.out.println("No Absen: 01");
+        System.out.println("Prodi\t: D-IV Teknik Informatika");
+        System.out.println("Alamat\t: Jl. Achmad Dahlan, Sananrejo, Turen, Malang");
     }
 }
