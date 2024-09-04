26fa675376fde4d8d7162d4fd9e9b573a73083e1
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2024-09-04 16:24:40 +0700

    Halo Linda + TTL

--- Biodata.java
+++ Biodata.java
@@ -2,6 +2,7 @@ public class Biodata{
     public static void main(String[] args){
         System.out.println("Perkenalan: ");
         System.out.println("\nNama\t: Linda octaviani");
+        System.out.println("TTL\t: Nganjuk, 29 Desember 2005");
         System.out.println("NIM\t: 244107020002");
         System.out.println("Kelas\t: TI-1A");
         System.out.println("No Absen: 15");
