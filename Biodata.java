fc2d05b84cac01d2938b555591098a98c7d4867a
febrianucup <muhammaddwifebrian@gmail.com>
2024-09-04 16:23:48 +0700

    perkenalan febri

--- Biodata.java
+++ Biodata.java
@@ -2,6 +2,7 @@ public class Biodata{
     public static void main(String[] args){
         System.out.println("Perkenalan: ");
         System.out.println("\nNama\t: Muhammad Dwi Febrian");
+        System.out.println("TTL\t: Situbondo, 17 Februari 2006");
         System.out.println("NIM\t: 244107020081");
         System.out.println("Kelas\t: TI 1-A");
         System.out.println("No Absen: 19");
